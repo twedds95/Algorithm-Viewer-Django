e82f7f70d3edff51c6fe1a0fd6b036151d12a3ee
Patrick Tweddell <patrick.tweddell@mail.mcgill.ca>
2020-04-22 16:28:39 -0400

    First comit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\user\Documents\Programming\Python\AlgorithmView\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
