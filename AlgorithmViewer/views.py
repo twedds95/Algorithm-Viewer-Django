4aa2ef3002ae6fd3040cd455b155e86d6678de78
Patrick Tweddell <patrick.tweddell@mail.mcgill.ca>
2020-04-23 19:01:44 -0400

    refactored code so the spacing changes with the number of bars

--- AlgorithmViewer/views.py
+++ AlgorithmViewer/views.py
@@ -4,13 +4,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .algorithms import generate_random_array
 
-size = 300
-number_list = generate_random_array(size, 500)
-numbers = []
-for i in range(size):
-    numbers.append({'numbers': number_list[i],
-                    'index': (i*3)})
+
+def generate_new_array():
+    size = 700
+    number_list = generate_random_array(size, 500)
+    numbers = []
+    for i in range(size):
+        numbers.append({'numbers': number_list[i],
+                        'index': (i * 900 / size)})
+    return numbers
 
 
 def algorithm_viewer(request):
+    numbers = generate_new_array()
     return render(request, 'AlgorithmViewer/ViewingWindow.html', {'numbers': numbers})
