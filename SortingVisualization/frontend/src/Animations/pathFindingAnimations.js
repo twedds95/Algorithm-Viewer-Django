9de73e438b94bffc64133f2e48b6c0772068272a
Patrick Tweddell <patrick.tweddell@mail.mcgill.ca>
2020-05-13 23:18:21 -0400

    added A* path finding, need to fix update

--- SortingVisualization/frontend/src/Animations/pathFindingAnimations.js
+++ SortingVisualization/frontend/src/Animations/pathFindingAnimations.js
@@ -2,16 +2,19 @@ import React from "react";
 import './pathFindingAnimations.css';
 import {getA_StarAnimations} from "../PathFindingAlgorithms/aStar";
 
-const PRIMARY_COLOR = '#b6adad';
-const START_COLOR = '#428bca';
+const PRIMARY_COLOR = '#e7e4e4';
+const START_COLOR = 'rgb(3,253,77)';
 const END_COLOR = '#c925e7';
 const WALL_COLOR = '#000000';
-const PATH_COLOR = 'rgba(3,253,77,0.47)';
-const SPEED = 0.7;
-const PROB_OF_WALL = 0.2;
-const NUMBER_OF_BOXES_WIDTH = 80;
-const NUMBER_OF_BOXES_HEIGHT = 40;
-const BOX_DIM = 10;
+const PATH_COLOR = '#428bca';
+const VISITED_COLOR = 'rgba(213,48,48,0.47)';
+const FAILURE_COLOR = 'rgb(255,0,0)';
+
+const SPEED = 50;
+const PROB_OF_WALL = 0.25;
+const NUMBER_OF_BOXES_WIDTH = 120;
+const NUMBER_OF_BOXES_HEIGHT = 60;
+const BOX_DIM = 6;
 const MARGIN = 2;
 
 const ALGORITHMS = ["A*"];
@@ -31,49 +34,36 @@ export default class PathFindingVisualizer extends React.Component {
     }
 
     resetGrid() {
-        let squares = [];
+        const grid = [];
+        //TODO: Reset not working for some reason, need to refresh every time
 
-        function generateTwoUniquePoints() {
-            let min = 0;
-            let xS = [];
-            let yS = [];
-            let points = {};
-            for (let i = 0; i < 2; i++) {
-                xS.push(Math.floor(Math.random() * (NUMBER_OF_BOXES_WIDTH - min) + min));
-                yS.push(Math.floor(Math.random() * (NUMBER_OF_BOXES_HEIGHT - min) + min));
-            }
-            if (xS[0] === xS[1] && yS[0] === yS[1]) {
-                points = generateTwoUniquePoints();
-            } else {
-                points = {startX: xS[0], startY: yS[0], endX: xS[1], endY: yS[1]};
-            }
-            return points;
-        }
-
-        let points = generateTwoUniquePoints();
-        let start = {isA: 'start', x: points.startX, y: points.startY};
-        let end = {isA: 'end', x: points.endX, y: points.endY};
+        // let points = generateTwoUniquePoints();
+        // let start = {isA: 'start', x: points.startX, y: points.startY};
+        // let end = {isA: 'end', x: points.endX, y: points.endY};
+        let start = {isA: 'start', x: 0, y: 0};
+        let end = {isA: 'end', x: NUMBER_OF_BOXES_WIDTH - 1, y: NUMBER_OF_BOXES_HEIGHT - 1};
         for (let i = 0; i < NUMBER_OF_BOXES_WIDTH; i++) {
             for (let j = 0; j < NUMBER_OF_BOXES_HEIGHT; j++) {
                 if (!((i === start.x && j === start.y) || (i === end.x && j === end.y))) {
                     if (Math.random() < PROB_OF_WALL) {
-                        squares.push({isA: 'wall', x: i, y: j});
+                        grid.push({isA: 'wall', x: i, y: j});
                     } else {
-                        squares.push({isA: 'free', x: i, y: j})
+                        grid.push({isA: 'free', x: i, y: j});
                     }
                 }
             }
         }
-        squares.push(start);
-        squares.push(end);
-        this.setState({grid: squares});
+        grid.push(start);
+        grid.push(end);
+
+        this.setState({grid});
     }
 
     animatePath() {
         let animations = [];
         switch (document.getElementById('algorithm').value) {
             case "A*":
-                animations = getA_StarAnimations(this.state);
+                animations = getA_StarAnimations(this.state.grid);
                 break;
 
             default:
@@ -81,7 +71,28 @@ export default class PathFindingVisualizer extends React.Component {
         }
 
         for (let i = 0; i < animations.length; i++) {
-            const squares = document.getElementsByClassName('grid-square');
+            let action = animations[i].action;
+            const square = animations[i].square;
+            const gridSquareID = (square.y * NUMBER_OF_BOXES_WIDTH) + square.x + 1;
+            const gridSquare = document.getElementById(gridSquareID);
+            const gridSquareStyle = gridSquare.style;
+            if (action === 'newPath') {
+                setTimeout(() => {
+                    gridSquareStyle.backgroundColor = PATH_COLOR;
+                }, i * SPEED);
+            } else if (action === 'visit') {
+                setTimeout(() => {
+                    gridSquareStyle.backgroundColor = VISITED_COLOR;
+                }, i * SPEED);
+            } else if (action === 'failure') {
+                setTimeout(() => {
+                    gridSquareStyle.backgroundColor = FAILURE_COLOR;
+                }, i * SPEED);
+            } else if (action === 'rebuild') {
+                setTimeout(() => {
+                    gridSquareStyle.backgroundColor = PATH_COLOR;
+                }, i * SPEED);
+            }
         }
     }
 
@@ -94,91 +105,82 @@ export default class PathFindingVisualizer extends React.Component {
     }
 
     renderTheGrid() {
+        const {grid} = this.state;
         let squares = [];
-        let grid = this.state.grid;
         for (let i = 0; i < grid.length; i++) {
-            {
-                let y = grid[i].y * (BOX_DIM + MARGIN)
-                let x = grid[i].x * (BOX_DIM + MARGIN)
-                switch (grid[i].isA) {
-                    case 'wall':
-                        squares.push(<div
-                            className="grid-square"
-                            style={{
-                                backgroundColor: WALL_COLOR,
-                                top: `${y}px`,
-                                left: `${x}px`,
-                                width: `${BOX_DIM}px`,
-                                height: `${BOX_DIM}px`,
-                                margin: `${MARGIN}px`,
-                            }}>
-                        </div>)
-                        break;
-                    case 'free':
-                        squares.push(<div
-                            className="grid-square"
-                            style={{
-                                backgroundColor: PRIMARY_COLOR,
-                                top: `${y}px`,
-                                left: `${x}px`,
-                                width: `${BOX_DIM}px`,
-                                height: `${BOX_DIM}px`,
-                                margin: `${MARGIN}px`,
-                            }}>
-                        </div>)
-                        break;
-                    case 'path':
-                        squares.push(<div
-                            className="grid-square"
-                            style={{
-                                backgroundColor: PATH_COLOR,
-                                top: `${y}px`,
-                                left: `${x}px`,
-                                width: `${BOX_DIM}px`,
-                                height: `${BOX_DIM}px`,
-                                margin: `${MARGIN}px`,
-                            }}>
-                        </div>)
-                        break;
-                    case 'start':
-                        squares.push(<div
-                            className="grid-square"
-                            style={{
-                                backgroundColor: START_COLOR,
-                                top: `${y}px`,
-                                left: `${x}px`,
-                                width: `${BOX_DIM}px`,
-                                height: `${BOX_DIM}px`,
-                                margin: `${MARGIN}px`,
-                            }}>
-                        </div>)
-                        break;
-                    case 'end':
-                        squares.push(<div
-                            className="grid-square"
-                            style={{
-                                backgroundColor: END_COLOR,
-                                top: `${y}px`,
-                                left: `${x}px`,
-                                width: `${BOX_DIM}px`,
-                                height: `${BOX_DIM}px`,
-                                margin: `${MARGIN}px`,
-                            }}>
-                        </div>)
-                        break;
-                    default:
-                        break;
-                }
+            let y = grid[i].y * (BOX_DIM + MARGIN)
+            let x = grid[i].x * (BOX_DIM + MARGIN)
+            let key = (grid[i].y * NUMBER_OF_BOXES_WIDTH) + grid[i].x + 1;
+            switch (grid[i].isA) {
+                case 'free':
+                    squares.push(<div
+                        className="grid-square"
+                        id={key}
+                        style={{
+                            backgroundColor: PRIMARY_COLOR,
+                            top: `${y}px`,
+                            left: `${x}px`,
+                            width: `${BOX_DIM}px`,
+                            height: `${BOX_DIM}px`,
+                            margin: `${MARGIN}px`,
+                        }}>
+                    </div>)
+                    break;
+                case 'wall':
+                    squares.push(<div
+                        className="grid-square"
+                        id={key}
+                        style={{
+                            backgroundColor: WALL_COLOR,
+                            top: `${y}px`,
+                            left: `${x}px`,
+                            width: `${BOX_DIM}px`,
+                            height: `${BOX_DIM}px`,
+                            margin: `${MARGIN}px`,
+                        }}>
+                    </div>)
+                    break;
+                case 'start':
+                    squares.push(<div
+                        className="grid-square"
+                        id={key}
+                        style={{
+                            backgroundColor: START_COLOR,
+                            top: `${y}px`,
+                            left: `${x}px`,
+                            width: `${BOX_DIM}px`,
+                            height: `${BOX_DIM}px`,
+                            margin: `${MARGIN}px`,
+                        }}>
+                    </div>)
+                    break;
+                case 'end':
+                    squares.push(<div
+                        className="grid-square"
+                        id={key}
+                        style={{
+                            backgroundColor: END_COLOR,
+                            top: `${y}px`,
+                            left: `${x}px`,
+                            width: `${BOX_DIM}px`,
+                            height: `${BOX_DIM}px`,
+                            margin: `${MARGIN}px`,
+                        }}>
+                    </div>)
+                    break;
+                default:
+                    break;
             }
+
         }
-        return squares
+        return squares;
     }
 
     render() {
         return (
             <div>
                 <h1> Visualize Path Finding Algorithms at Work</h1>
-                <button onClick={() => this.resetGrid()}>Generate New Grid</button>
+                <button onClick={() =>this.resetGrid()}>Generate New Grid</button>
                 <label>Select Path Finding Algorithm: </label>
                 <select className="m-2" id="algorithm">
                     {this.createSelection()}
@@ -190,4 +192,21 @@ export default class PathFindingVisualizer extends React.Component {
             </div>
         );
     }
+}
+
+function generateTwoUniquePoints() {
+    let min = 0;
+    let xS = [];
+    let yS = [];
+    let points;
+    for (let i = 0; i < 2; i++) {
+        xS.push(Math.floor(Math.random() * (NUMBER_OF_BOXES_WIDTH - min) + min));
+        yS.push(Math.floor(Math.random() * (NUMBER_OF_BOXES_HEIGHT - min) + min));
+    }
+    if (xS[0] === xS[1] && yS[0] === yS[1]) {
+        points = generateTwoUniquePoints();
+    } else {
+        points = {startX: xS[0], startY: yS[0], endX: xS[1], endY: yS[1]};
+    }
+    return points;
 }
--- SortingVisualization/frontend/src/PathFindingAlgorithms/aStar.js
+++ SortingVisualization/frontend/src/PathFindingAlgorithms/aStar.js
@@ -1,5 +1,167 @@
 export function getA_StarAnimations(squares) {
     const animations = [];
-    if (squares.length <= 1) return [];
+    if (squares.length <= 1) return animations;
+    pathHelper(squares, animations);
     return animations;
+}
+
+function pathHelper(squares, animations) {
+    let start = squares.filter(function (ele) {
+        if (ele.isA === 'start') {
+            return ele;
+        } else {
+            return null;
+        }
+    })[0];
+    let end = squares.filter(function (ele) {
+        if (ele.isA === 'end') {
+            return ele;
+        } else {
+            return null;
+        }
+    })[0];
+    start.gScore = 0;
+    start.fScore = heuristic(start, end);
+    let openSet = [];
+    openSet.push(start);
+    let previous = start;
+    while (openSet.length > 0) {
+        let current = openSet[0];
+        for (let i = 1; i < openSet.length; i++) {
+            if (openSet[i].fScore < current.fScore) {
+                current = openSet[i];
+            }
+        }
+        if (current.previous !== previous && current !== start) {
+            findLastCommonPoint(current, previous, animations);
+        }
+        if (current !== start && current !== end) {
+            current.isA = 'path';
+            animations.push({action: 'newPath', square: current});
+        }
+        previous = current;
+        if (current === end) {
+            return;
+        }
+        openSet = arrayRemove(openSet, current);
+        let neighbours = [];
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                let neighbour = squares.filter(function (ele) {
+                    if (ele.x === current.x + i && ele.y === current.y + j
+                        && (ele.isA === 'free' || ele.isA === 'end' || ele.isA === 'wall')
+                    ) {
+                        return ele;
+                    } else {
+                        return null;
+                    }
+                })[0];
+                if (!neighbour) {
+                    neighbour = {isA: 'offMap'};
+                }
+                neighbours.push(neighbour);
+            }
+        }
+        if (neighbours[1].isA === 'wall' && neighbours[3].isA === 'wall') {
+            neighbours[0].isA = 'wall';
+        }
+        if (neighbours[1].isA === 'wall' && neighbours[5].isA === 'wall') {
+            neighbours[2].isA = 'wall';
+        }
+        if (neighbours[3].isA === 'wall' && neighbours[7].isA === 'wall') {
+            neighbours[6].isA = 'wall';
+        }
+        if (neighbours[5].isA === 'wall' && neighbours[7].isA === 'wall') {
+            neighbours[8].isA = 'wall';
+        }
+
+        let possibilities = [];
+        for (let i = 0; i < neighbours.length; i++) {
+            if (neighbours[i].isA !== "wall" && neighbours[i].isA !== "offMap") {
+                possibilities.push(neighbours[i]);
+            }
+        }
+
+        for (let i = 0; i < possibilities.length; i++) {
+            let neighbour = possibilities[i];
+            let tempScore = current.gScore + weight(current, neighbour, end);
+            if (!neighbour.gScore || tempScore < neighbour.gScore) {
+                neighbour.gScore = tempScore;
+                neighbour.fScore = tempScore + heuristic(neighbour, end);
+                neighbour.previous = current;
+                if (!openSet.includes(neighbour)) {
+                    // if (neighbour !== start && neighbour !== end) {
+                    //     animations.push({action: 'visit', square: neighbour});
+                    // }
+                    openSet.push(neighbour);
+                }
+            }
+        }
+    }
+    let failure = squares.filter(function (ele) {
+        if (ele.isA === 'path') {
+            return ele;
+        } else {
+            return null;
+        }
+    });
+    for (let i = 0; i < failure.length; i++) {
+        animations.push({action: 'failure', square: failure[i]});
+    }
+}
+
+function heuristic(position, end) {
+    let x1 = position.x;
+    let y1 = position.y;
+    let x2 = end.x;
+    let y2 = end.y;
+    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    // return Math.abs((x2 - x1)) + Math.abs((y2 - y1));
+}
+
+function arrayRemove(arr, value) {
+    return arr.filter(function (ele) {
+        return ele !== value;
+    });
+}
+
+function weight(current, neighbour, end) {
+    let w;
+    if (end.x === neighbour.x || end.y === neighbour.y) {
+        w = 0.9;
+    } else if (current.x === neighbour.x || current.y === neighbour.y) {
+        w = 1;
+    } else {
+        w = 0.9;
+    }
+    return w;
+}
+
+function findLastCommonPoint(current, previous, animations) {
+    let oldPath = [];
+    let newPath = [];
+    let old = previous;
+    let newTop = current;
+    while (previous) {
+        oldPath.push(previous);
+        previous = previous.previous
+    }
+    while (current) {
+        newPath.push(current);
+        current = current.previous
+    }
+
+    let commonElements = newPath.filter(value => oldPath.includes(value));
+    while (!commonElements.includes(old)) {
+        animations.push({action: 'visit', square: old});
+        old = old.previous;
+    }
+    let rebuildPath = [];
+    while (!commonElements.includes(newTop)) {
+        rebuildPath.unshift(newTop);
+        newTop = newTop.previous;
+    }
+    for (let i = 0; i < rebuildPath.length; i++) {
+        animations.push({action: 'rebuild', square: rebuildPath[i]});
+    }
 }
