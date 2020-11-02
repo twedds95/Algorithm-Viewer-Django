ffa377af0192457e25b3d10a0cd17c64f30dd98b
Patrick Tweddell <patrick.tweddell@mail.mcgill.ca>
2020-11-02 18:07:33 -0500

    Updates

---
+++ precache-manifest.f778f6d7c1f4d69bfef851801434fcd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf58bd69c71562ab5bb49536c7cf00e",
+    "url": "/Algorithm-Viewer/index.html"
+  },
+  {
+    "revision": "9bbd01766cfd4d4aa52b",
+    "url": "/Algorithm-Viewer/static/css/main.d7d72183.chunk.css"
+  },
+  {
+    "revision": "f2a41750c299cd888305",
+    "url": "/Algorithm-Viewer/static/js/2.e7075a2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Algorithm-Viewer/static/js/2.e7075a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bbd01766cfd4d4aa52b",
+    "url": "/Algorithm-Viewer/static/js/main.cdff6335.chunk.js"
+  },
+  {
+    "revision": "0a0184690b5b76ff3307",
+    "url": "/Algorithm-Viewer/static/js/runtime-main.cc9dc729.js"
+  }
+]);
