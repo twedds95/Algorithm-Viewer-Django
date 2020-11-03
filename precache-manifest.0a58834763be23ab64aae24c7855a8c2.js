46d6fb410d78f92833c1abceb3109b4f16639833
Patrick Tweddell <patrick.tweddell@mail.mcgill.ca>
2020-11-03 13:40:24 -0500

    Updates

--- precache-manifest.0a58834763be23ab64aae24c7855a8c2.js
+++ precache-manifest.0a58834763be23ab64aae24c7855a8c2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "be60d5d1df7bb64ea52354cfd93bd687",
+    "revision": "6cae0e4a0a62eedbffe7f0c3bc50cc89",
     "url": "/Algorithm-Viewer/index.html"
   },
   {
-    "revision": "43a7058affb5507b37ec",
+    "revision": "4f8437b41712ffb78cc1",
     "url": "/Algorithm-Viewer/static/css/main.9cc845ce.chunk.css"
   },
   {
-    "revision": "d3a767281acea7265312",
-    "url": "/Algorithm-Viewer/static/js/2.902fb154.chunk.js"
+    "revision": "246009e65d8e0ba4a021",
+    "url": "/Algorithm-Viewer/static/js/2.325572cb.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/Algorithm-Viewer/static/js/2.902fb154.chunk.js.LICENSE.txt"
+    "url": "/Algorithm-Viewer/static/js/2.325572cb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "43a7058affb5507b37ec",
-    "url": "/Algorithm-Viewer/static/js/main.43520758.chunk.js"
+    "revision": "4f8437b41712ffb78cc1",
+    "url": "/Algorithm-Viewer/static/js/main.0a837112.chunk.js"
   },
   {
     "revision": "0a0184690b5b76ff3307",
